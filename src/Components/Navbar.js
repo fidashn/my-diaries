592ac5318b986f07f57a30e9ff92dac08afe51ce
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-26 21:14:43 +0300

    created components

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <li>Nav item</li>
+      <li>Nav item</li>
+      <li>Nav item</li>
+      <li>Nav item</li>
+    </div>
+  )
+}
+
+export default Navbar
