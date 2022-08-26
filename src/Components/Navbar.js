b524e9320c4e225da320bc75b20d43181b9d0978
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-26 21:35:32 +0300

    added ul to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,10 +3,12 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='navbar'>
+      <ul className='navbar-items'>
       <li>Nav item</li>
       <li>Nav item</li>
       <li>Nav item</li>
       <li>Nav item</li>
+      </ul>
       <button className='sign-inup btn'>Sign Up</button>
       <button className='sign-in btn'>Sign In</button>
     </div>
