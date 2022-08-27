2a340b261f6322836c7b7ef22e6ce1ec8376c273
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-27 20:36:54 +0300

    installed react router npm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import { BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <BrowserRouter >
+      <Route path="/home" component={Home} />
+      {/* <Route path="/signUp" component={SignUp} />
+      <Route exact path="/signIn" component={SignIn} /> */}
+      </BrowserRouter>
       <Footer />
     </div>
   );
