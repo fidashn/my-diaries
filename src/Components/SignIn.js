13cc06a3e4a3b1d44c8f025a50ae27c9aa4296b6
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-28 15:28:23 +0300

    created the sign in form

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function SignIn() {
   return (
-    <div>SignIn</div>
+    <div>
+    <form action="" className='signInForm'>
+    Email:
+    <input type="text" placeholder="Email.."/>
+    Password:
+    <input type="text" placeholder="Password.."/>
+    </form>
+  </div>
   )
 }
 
